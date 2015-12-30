57d252b347ea5e1124c4714f5c19cfc63fef7af5
Mike Clarke <mike.k.clarke@gmail.com>
2015-12-30 15:13:57 -0800

    Add Oracle FDW 1.3.0 formula

---
+++ oracle-fdw.rb
@@ -0,0 +1,16 @@
+class OracleFdw < Formula
+  desc "Oracle FDW Postgres Extension"
+  homepage "http://laurenz.github.io/oracle_fdw/"
+  url "http://api.pgxn.org/dist/oracle_fdw/1.3.0/oracle_fdw-1.3.0.zip"
+  sha256 "15dcc6ec960f8eaed24036eb2fec902fe8a257e415e2aa127e679149b732af1c"
+
+  depends_on "oracle-client"
+  depends_on "oracle-headers"
+  depends_on "postgresql"
+
+  def install
+    pg_config = "#{Formula["postgresql"].opt_bin}/pg_config"
+    system "make", "PG_CONFIG=#{pg_config}"
+    system "make", "PG_CONFIG=#{pg_config}", "install"
+  end
+end
